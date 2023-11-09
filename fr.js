7fd4f41bf66d693ba85d2dff4eb63aa0e038fbe3
maddypp <113092719+maddypp@users.noreply.github.com>
2023-11-10 07:38:26 +1100

    Add files via upload

---
+++ fr.js
@@ -0,0 +1,45 @@
+img = "";
+status = "";
+object = [];
+
+function preload() {
+    img = loadImage("fr.jpeg");
+}
+
+function setup() {
+    canvas = createCanvas(600, 400);
+    canvas.center();
+    objectdetector = ml5.objectDetector('cocossd' , modelloaded )
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelloaded() {
+    console.log("model is loaded");
+    status = true;
+    objectdetector.detect(img, gotresults);
+}
+
+function gotresults(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        object = results;
+    }
+}
+
+function draw() {
+    image(img, 0, 0, 600, 400);
+    if (status != "") {
+        for (i=0; i<object.length; i++) {
+            document.getElementById("status").innerHTML = "Loading...";
+            fill("#ff1100");
+            percent = floor(object[i].confidence * 100);
+            text(object[i].label + "" + percent + "%", object[i].x, object[i].y);
+            noFill();
+            stroke("#ff1100");
+            rect(object[i].x, object[i].y, object[i].width, object[i].height);
+        }
+    }
+}
